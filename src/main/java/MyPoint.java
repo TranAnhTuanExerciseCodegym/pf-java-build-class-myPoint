25ac30d87b1503d1336ddc6d51dcb0826d3c5f76
Anh Tuan <tincanxh@gmail.com>
2018-07-12 22:49:28 +0700

    xay dung lop Mypoint

---
+++ src/main/java/MyPoint.java
@@ -0,0 +1,32 @@
+public class MyPoint {
+    private double x = 0;
+    private double y = 0;
+
+    public MyPoint() {
+    }
+
+    public MyPoint(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double distance(double x1, double y1, double x2, double y2) {
+        return Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+    }
+}
---
+++ src/main/java/MyPointTest.java
@@ -0,0 +1,6 @@
+public class MyPointTest {
+    public static void main(String[] args) {
+        MyPoint myPoint = new MyPoint();
+        System.out.println(myPoint.distance(0, 0, 10, 30.5));
+    }
+}
